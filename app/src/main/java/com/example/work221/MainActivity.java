3f5e29f5cb1abc9c78f16deeea56ed1470b7db24
yarchike <56361599+yarchike@users.noreply.github.com>
2020-01-25 15:24:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/work221/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.work221;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.ActivityManager;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText mInputNote;
+    private Button mBtnSaveNote;
+    private SharedPreferences myNoteSharedPref;
+    private static String NOTE_TEXT = "note_text";
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        initViews();
+        getDateFromSharedPref();
+    }
+
+    private void initViews() {
+        mInputNote = findViewById(R.id.inputNote);
+        mBtnSaveNote = findViewById(R.id.btnSaveNote);
+        myNoteSharedPref = getSharedPreferences("Myfile", MODE_PRIVATE);
+
+
+        mBtnSaveNote.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences.Editor myEditor = myNoteSharedPref.edit();
+                String noteTXT = mInputNote.getText().toString();
+                myEditor.putString(NOTE_TEXT, noteTXT);
+                Boolean p = myEditor.commit();
+                if (p) {
+                    Toast.makeText(MainActivity.this, getText(R.string.save_data), Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(MainActivity.this, getText(R.string.nosave_data), Toast.LENGTH_LONG).show();
+                }
+
+            }
+        });
+    }
+    private void getDateFromSharedPref(){
+        String noteTxt = myNoteSharedPref.getString(NOTE_TEXT, "");
+        mInputNote.setText(noteTxt);
+    }
+}
